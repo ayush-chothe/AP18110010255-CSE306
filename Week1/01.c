d4b84a0ac8d50e1e9af097e179bc2b6628975b21
ayush-chothe <53037930+ayush-chothe@users.noreply.github.com>
2020-08-09 22:33:09 +0530

    Test

---
+++ Week1/01.c
@@ -0,0 +1,10 @@
+/* Implementation of Language recognizer for set of all strings over input alphabet ∑={a,b}
+    containing even number of a’s and even number of b’s. */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Test");
+    return 0;
+}
